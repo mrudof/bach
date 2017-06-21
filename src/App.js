112a76d485f26434746ba8086def817f7861f2f8
Katie Quinn <katiequinn@KQUINN-MBP-CHI.local>
2017-06-21 11:10:48 -0500

    Adding random 16 bachs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import List from './List';
 import contestantData from './contestant-data';
 import Nav from './Nav';
-import { footer } from 'bootstrap';
 
 class App extends Component {
   render() {
+    const n = 16;
+    const shuffled = contestantData.sort(() => .5 - Math.random());// shuffle  
+    let selected = shuffled.slice(0,n);
+    selected.forEach(function(item){ 
+      item.clicked = 0;
+    });
     return (
       <div className="App">
         <Nav/>
-        <List contestants={contestantData}/>
+        <List contestants={selected}/>
         <footer>
 		     <p className="disclaimer">All Bachelor photos are property of their respective owners; No Copyright infringement intended.</p>
 	      </footer>
