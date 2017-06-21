ad2d06de561b37d89dcdf18da925a85b18d0815e
Katie Quinn <katiequinn@KQUINN-MBP-CHI.local>
2017-06-21 15:12:51 -0500

    Navbar styling in progress

--- src/Nav.js
+++ src/Nav.js
@@ -9,15 +9,21 @@ class Nav extends Component {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="#">
-              <img alt="logo" className="bachelor-logo" src="The_Bachelor_usa_logo.png"/>
-            </a>
+          <div className="roseBackground">
+              <img className="rosePic" src="https://www.freewebheaders.com/wordpress/wp-content/gallery/roses/red-rose-flower-petals-background-header.jpg"/>
+              <div>
+                <a href="#">
+                  <img alt="logo" className="bachelor-logo" src="The_Bachelor_usa_logo.png"/>
+                </a>
+                <div className="navOptions">
+                  <ul className="nav navbar-nav navbar-right navbar-buttons">
+                  <li><Score/></li>
+                  <li><button onClick={this.handleClick} type="play" className="btn btn-danger resetButton"><span className="glyphicon glyphicon-repeat" aria-hidden="true"></span>
+</button></li>
+                </ul>
+                </div>
+              </div>
           </div>
-          <ul className="nav navbar-nav navbar-right navbar-buttons">
-            <li><Score/></li>
-            <li><button onClick={this.handleClick} type="play" className="btn btn-danger">Restart</button></li>
-          </ul>
         </div>
       </nav>
     );
