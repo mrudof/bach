ef34e1f2a3697c3e847c7d1d7c18e34707fb4446
Brittany Harrington <brittanyharrington@BHARRINGTON-MBP-HOU.local>
2017-06-21 15:36:11 -0500

    unsure

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import contestantData from './contestant-data';
 import Nav from './Nav';
 
 class App extends Component {
-  render() {
-    const n = 16;
-    const shuffled = contestantData.sort(() => .5 - Math.random());// shuffle  
-    let selected = shuffled.slice(0,n);
-    selected.forEach(function(item){ 
-      item.clicked = 0;
-    });
-    return (
-      <div className="App">
-        <Nav/>
-        <List contestants={selected}/>
-        <div className="footer navbar-bottom">
-		     <p className="disclaimer">All Bachelor photos are property of their respective owners; No Copyright infringement intended.</p>
-	      </div>
-      </div>
-    );
-  }
+    render() {
+        const n = 16;
+        const shuffled = contestantData.sort(() => .5 - Math.random()); // shuffle  
+        let selected = shuffled.slice(0, n);
+        selected.forEach(function(item) {
+            item.clicked = 0;
+        });
+        return ( <
+            div className = "App" >
+            <
+            Nav / >
+            <
+            List contestants = { selected }
+            /> <
+            div className = "footer navbar-bottom" >
+            <
+            p className = "disclaimer" > All Bachelor photos are property of their respective owners; No Copyright infringement intended. < /p> < /
+            div > <
+            /div>
+        );
+    }
 }
 
 export default App;
