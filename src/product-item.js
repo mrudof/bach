0fcea5f6607f7ab1ee86f7e8e10604d3e4a54e79
Brittany Harrington <brittanyharrington@BHARRINGTON-MBP-HOU.local>
2017-06-21 11:00:14 -0500

    Grid of pictures finally working

--- src/product-item.js
+++ src/product-item.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
 class ProductItem extends Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.name}</td>
-        <td>
-          <div className="pictures">
-            <img src={this.props.picture} alt="faces" className={this.props.season}></img>
-          </div>
-        </td>
-      </tr>
-    )
-  }
+    render() {
+        return (
+            /*<tr>
+              <td>{this.props.name}</td>
+              <td>
+                <div className="pictures">
+                  <img src={this.props.picture} alt="faces" className={this.props.season}></img>
+                </div>
+              </td>
+            </tr>*/
+            <
+            div >
+            <
+            div key = "name" > { this.props.name } < /div>    <
+            div className = "pictures"
+            key = "picture" > < img src = { this.props.picture }
+            className = { this.props.season }
+            /> </div >
+
+            <
+            /div >
+        )
+    }
 }
 
 export default ProductItem;
