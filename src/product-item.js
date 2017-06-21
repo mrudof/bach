25d0ad7ae0ea9fb5468adf252faa17171c161a34
Katie Quinn <katiequinn@KQUINN-MBP-CHI.local>
2017-06-21 13:19:10 -0500

    Formatting

--- src/product-item.js
+++ src/product-item.js
@@ -4,10 +4,10 @@ class ProductItem extends Component {
     render() {
         return (
             <div>
-              <div key = "name" > { this.props.name } </div>
               <div className = "pictures" key = "picture">
                 <img src = { this.props.picture } className = { this.props.season }/> 
               </div>
+              <div className = "centerText" key = "name" > { this.props.name } </div>
             </div>
         )
     }
