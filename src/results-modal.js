9b713543482641570d671d30fd46acbfe5e4e032
Dang32 <debnag35@gmail.com>
2017-06-22 11:17:18 -0500

    Fix results modal

--- src/results-modal.js
+++ src/results-modal.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import { Modal } from 'react-bootstrap';
-<<<<<<< HEAD:src/results-modal.js
 class ResultsModal extends Component {
       constructor(props){
-=======
-
-class LosingModal extends Component {
-    constructor(props) {
->>>>>>> a2ceaca263bd9eb99d9729a0ee9cc0b89452fd2d:src/losing-modal.js
         super(props);
         this.state = {
             showModal: true
@@ -18,39 +12,34 @@ class LosingModal extends Component {
     close() {
         this.setState({ showModal: false });
     }
-<<<<<<< HEAD:src/results-modal.js
   render(){
-    var result = this.props.score > 8 ? "You Won!" : "You Lost!";
+    let result, giphy;
+    if (this.props.score > 0){
+        result = "You Won!";
+        giphy = 'https://media.giphy.com/media/3o6ZsYfXaDrmmASwQo/giphy.gif';
+    }
+    else{
+        result = "You Lost!";
+        giphy = 'https://media.giphy.com/media/gcp6ZK9xFR0Nq/giphy.gif';
+    }
     return(
       <Modal onHide={this.close} show={this.state.showModal}>
         <Modal.Header closeButton>
-                <Modal.Title>{result} + {this.props.score}</Modal.Title>
+                <Modal.Title>
+                    <h1 className="gameOverTitle">
+                        {result}
+                    </h1>
+                </Modal.Title>
         </Modal.Header>
+        <Modal.Body> 
+            <span className="gameOverTitle">{"Final Score: " + this.props.score}</span>
+            <div className = "gameOverGif">
+                <img src = {giphy}/> 
+            </div> 
+        </Modal.Body >
       </Modal>
     );
   }
-=======
-    render() {
-        return ( <
-            Modal onHide = { this.close }
-            show = { this.state.showModal } >
-            <
-            Modal.Header closeButton >
-            <
-            Modal.Title > < h1 className = "gameOverTitle" > You Lost! < /h1>< /Modal.Title > < /
-            Modal.Header > <
-            Modal.Body > < div className = "gameOverGif" >
-            <
-            img src = {
-                'https://media.giphy.com/media/gcp6ZK9xFR0Nq/giphy.gif'
-            }
-            / > </div > < /Modal.Body >
-
-            <
-            /Modal>
-        );
-    }
->>>>>>> a2ceaca263bd9eb99d9729a0ee9cc0b89452fd2d:src/losing-modal.js
 }
 
 export default ResultsModal;
