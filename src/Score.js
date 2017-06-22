69dae15a39adde3d5f8836edc4ef5963608f1812
Brittany Harrington <brittanyharrington@BHARRINGTON-MBP-HOU.local>
2017-06-22 10:30:59 -0500

    Centered buttons, merged changes

--- src/Score.js
+++ src/Score.js
@@ -4,24 +4,25 @@ import LosingModal from './losing-modal';
 
 
 class Score extends Component {
-  render() {
-    let modal;
-    if(this.props.count === 16) {
-      if(this.props.score > 8) {
-        modal = (<WinningModal/>)
-      } else {
-        modal = (<LosingModal/>)
-      }
+    render() {
+        let modal;
+        if (this.props.count === 16) {
+            if (this.props.score > 8) {
+                modal = ( < WinningModal / > )
+            } else {
+                modal = ( < LosingModal / > )
+            }
+        }
+        return ( <
+            div >
+            <
+            div className = "score" >
+            Score: { this.props.score }
+            / 16 <
+            /div> { modal } <
+            /div>
+        );
     }
-    return (
-    <div>
-      <div className="score">
-        Score: {this.props.score} / 16
-      </div>
-      {modal}
-    </div>
-    );
-  }
 }
 
 export default Score;
